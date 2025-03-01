d0f416c7d97f699a711406cb67745f6df8065f64
Mahdi-CSE <mahdi.cse.bd@gmail.com>
2025-03-01 06:16:39 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// The shortcut for tailwindcss
+/**@type {import('tailwindcss').config} */
+module.exports = {
+    content: ["./src/**/*{html,js}"],
+    theme: {
+        extend : {},
+    },
+    plugins: [],
+}
